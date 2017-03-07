e9ad26fd0aa5135043b7fb3d55edf33e8df245b4
LucasMcL <LMcLaughlin09@gmail.com>
2017-03-07 09:15:36 -0600

    completed assignment

--- streams.js
+++ streams.js
@@ -1,17 +1,31 @@
 #!/usr/bin/env node
-const { Readable } = require('stream')
-const rs = new Readable() // rs = readstream
-rs.push('line1 ')
-rs.push('line2')
-rs.push(null)
+const { Readable, Writable, Transform } = require('stream')
+const readStream = new Readable()
+const writeStream = new Writable()
+const transformStream = new Transform()
+let i = 0
 
-rs.on('data', buffer => {
-	process.stdout.write(`received chunk: ${buffer.toString()}\n`)
-})
+readStream._read = () => {
+  if (i > 100) {
+    readStream.push(null) // when null is pushed to the stream, it emits an 'end' event
+  }
+  else {
+    readStream.push(`${i++}`)  // have to make i a string. Streams only handle strings and buffers
+  }
+}
 
-rs.on('end', () => process.stdout.write('end of stream\n'))
+// writeStream._write = (buffer, _, cb) => {
+//   process.stdout.write(`${buffer.toString()}\n`)
+//   cb()
+// }
 
+transformStream._transform = function(buffer, _, cb) {
+	output = Number(buffer) / 2 + 7
+	// process.stdout.write(`${output}\n`)
+	cb(null, `${output.toString()}\n`)
+}
 
+readStream.pipe(transformStream).pipe(process.stdout)
 
 
 
